0bcba058f77f6c100a3b61651bcb77f074d0439f
Haku17113 <riku0925h@gmail.com>
2019-06-16 13:23:45 +0900

    add: ページルーティングの導入
    createStoreを別ファイルに切り出した。

---
+++ src/createStore.js
@@ -0,0 +1,23 @@
+import { createStore as reduxCreateStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import logger from 'redux-logger';
+import * as reducers from './reducers';
+
+export default function createStore(history){
+  const rootReducer = combineReducers({
+    ...reducers,
+    router : connectRouter(history),
+  });
+  const savedState = JSON.parse(localStorage.getItem('app-state'));
+
+  return reduxCreateStore(
+    rootReducer,
+    savedState ? savedState : rootReducer(undefined, {type: 'INIT'}),
+    compose(
+      applyMiddleware(
+        routerMiddleware(history),
+        logger,
+      )
+    )
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
+import { ConnectedRouter } from 'connected-react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import './index.css';
 import App from './App';
-import * as reducers from './reducers';
+import createStore from './createStore';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(
-  combineReducers(reducers),
-  applyMiddleware(logger)
-);
+const history = createBrowserHistory();
+const store = createStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
