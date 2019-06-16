5899bd6f9ed9cfcd8c9491ce02946ae33c28f3ac
Haku17113 <riku0925h@gmail.com>
2019-06-16 15:11:41 +0900

    update: RankingコンポーネントをFunctional ComponetからClass Componetに変更

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Ranking({ categoryID }){
-  return (
-    <div>
-      <h1>Ranking</h1>
-      <p>カテゴリーID: {categoryID}</p>
-    </div>
-  )
+export default class Ranking extends React.Component{
+  componentWillMount(){
+    this.props.onMount(this.props.categoryID);
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(this.props.categoryID != nextProps.categoryID){
+      this.props.onUpdate(this.props.categoryID);
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Ranking</h1>
+        <p>カテゴリーID: {this.props.categoryID}</p>
+      </div>
+    )
+  }
 }
 
 Ranking.propTypes = {
-  categoryID: PropTypes.string
+  categoryID: PropTypes.string,
+  onMount: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired
 };
 Ranking.defaultProps = {
   categoryID: '1'
