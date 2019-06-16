9cb5642888af95c612711279a42afb4739802558
Haku17113 <riku0925h@gmail.com>
2019-06-16 16:03:42 +0900

    add: Container Componentを作成

---
+++ src/containers/Ranking.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import Ranking from '../components/Ranking';
+import * as actions from '../actions/Ranking';
+
+const mapStateToProps = (state, ownProps) => ({
+    categoryID: ownProps.categoryID
+});
+
+const mapDispatchToProps = dispatch => ({
+  onMount(categoryID){
+    dispatch(actions.fetchRanking(categoryID));
+  },
+  onUpdate(categoryID){
+    dispatch(actions.fetchRanking(categoryID));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ranking);
