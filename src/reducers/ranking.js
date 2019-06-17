87388371e6cd6983dad82e4f54b2fec0df81265e
Haku17113 <riku0925h@gmail.com>
2019-06-17 10:24:36 +0900

    hotfix: actionで渡すデータを生成するgetRankingを修正

--- src/reducers/ranking.js
+++ src/reducers/ranking.js
@@ -1,6 +1,6 @@
 const getRanking = response => {
   const ranking = [];
-  const itemLength = response.ResultSet.totalResultReturned;
+  const itemLength = response.ResultSet.totalResultsReturned;
   for(let index = 0; index < itemLength; index++){
     const item = response.ResultSet['0'].Result[index + ''];
     ranking.push({
@@ -25,7 +25,7 @@ export default (state = initialStore, action) => {
     case 'START_REQUEST':
       return {
         category: action.payload.category,
-        reanking: undefined,
+        ranking: undefined,
         error: false
       };
     case 'RECEIVE_DATA':
