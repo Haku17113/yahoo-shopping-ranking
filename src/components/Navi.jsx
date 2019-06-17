40b896200de3a9c3f59df4c252d372e7bbfe309a
Haku17113 <riku0925h@gmail.com>
2019-06-17 23:11:05 +0900

    update: NaviをLinkからDrawerに変更

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import PropTypes from 'prop-types';
 
-export default function Navi({ categories }){
+export default function Navi({ categories, onClick }){
   const to = category => (
     category.id === '1'
       ? '/all'
@@ -10,15 +13,19 @@ export default function Navi({ categories }){
   );
 
   return (
-    <ul>
-      {categories.map(category => (
-        <li key={`navi-item-${category.id}`}>
-          <Link to={to(category)}>
-            {category.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <Drawer variant="permanent">
+      <List style={{ width: 240 }}>
+        {categories.map(category => (
+          <ListItem
+            button
+            key={`navi-item-${category.id}`}
+            onClick={() => onClick(to(category))}
+          >
+            <ListItemText primary={category.name} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 }
 
@@ -28,5 +35,6 @@ Navi.propTypes = {
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired
     })
-  ).isRequired
+  ).isRequired,
+  onClick: PropTypes.func.isRequired
 };
--- src/containers/Navi.js
+++ src/containers/Navi.js
@@ -1,8 +1,15 @@
 import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
 import Navi from '../components/Navi';
 
 const mapStateToProps = state => ({
     categories: state.shopping.categories
 });
 
-export default connect(mapStateToProps)(Navi);
+const mapDispatchToProps = dispatch => ({
+    onClick(path){
+      dispatch(push(path));
+    }
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navi);
